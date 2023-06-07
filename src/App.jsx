aae2e0989a641ea6deb3d924ba17491c22bec27d
Rafael <rlag@outlook.com>
2023-06-06 21:40:59 -0300

    Commit inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Leiout />}/>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
